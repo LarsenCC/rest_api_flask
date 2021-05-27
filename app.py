95d0f02c493560be18ee860644cd7fec7983c9b8
LarsenCC <47952756+LarsenCC@users.noreply.github.com>
2021-05-27 13:47:23 +0200

    test heroku

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import os
 
 from flask import Flask
 from flask_restful import Api
-# from flask_jwt import JWT
+from flask_jwt import JWT
 from flask_jwt_extended import JWTManager
 
-# from security import authenticate, identity
+from security import authenticate, identity
 from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
@@ -33,10 +33,10 @@ app.secret_key = "jose" # app.config['JWT_SECRET_KEY']
 api = Api(app)
 
 # new endpoint /auth, we send it a username and a password
-# jwt = JWT(app, authenticate, identity)
+jwt = JWT(app, authenticate, identity)
 
 # JWT extended!
-jwt = JWTManager(app) # not creating /auth endpoint!
+# jwt = JWTManager(app) # not creating /auth endpoint!
 
 
 # we dont have to make the decorator ourselves
