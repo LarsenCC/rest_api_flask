382d167f0fec871e7a4c51de32c2b6d0cdfa6bbc
LarsenCC <47952756+LarsenCC@users.noreply.github.com>
2021-05-27 13:03:11 +0200

    comment security import

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_restful import Api
 # from flask_jwt import JWT
 from flask_jwt_extended import JWTManager
 
-from security import authenticate, identity
+# from security import authenticate, identity
 from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
